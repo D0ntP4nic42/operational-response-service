ec1dce59845669950de7d46892eca7b9def9a596
Leonardo <leonardo.gomes@cade.gov.br>
2026-09-17 09:32:05 -0300

    chore: inicialização do projeto

---
+++ operational-response-service/src/main/java/br/petroedge/oprs/OperationalResponseServiceApplication.java
@@ -0,0 +1,13 @@
+package br.petroedge.oprs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperationalResponseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperationalResponseServiceApplication.class, args);
+	}
+
+}
---
+++ operational-response-service/src/test/java/br/petroedge/oprs/OperationalResponseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.petroedge.oprs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperationalResponseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
