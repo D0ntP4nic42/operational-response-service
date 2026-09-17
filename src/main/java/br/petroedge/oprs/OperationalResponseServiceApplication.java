8a25bc58802865360e7ab4a89cd07f96cdf79841
Leonardo <leonardo.gomes@cade.gov.br>
2026-09-17 09:54:41 -0300

    fix: correções do spotless

--- src/main/java/br/petroedge/oprs/OperationalResponseServiceApplication.java
+++ src/main/java/br/petroedge/oprs/OperationalResponseServiceApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OperationalResponseServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OperationalResponseServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(OperationalResponseServiceApplication.class, args);
+    }
 }
